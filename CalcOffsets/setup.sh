447576a1fd405859413eb8c4a2a5be960a2ce859
lkashur <59710924+lkashur@users.noreply.github.com>
2024-08-05 15:51:20 -0500

    Delete CalcOffsets directory

--- CalcOffsets/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash                                                                                                                                             
-source /cvmfs/sbnd.opensciencegrid.org/products/sbnd/setup_sbnd.sh
-kx509                                                                                                                                                  
-voms-proxy-init -noregen -rfc -voms fermilab:/fermilab/sbnd/Role=Analysis -valid 168:00                                                                
-setup sbndcode v09_83_01 -q e26:prof
-make
